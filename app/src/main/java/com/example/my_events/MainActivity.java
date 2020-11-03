d49a4ee090adc9d6903a2b42266579235c862eb4
VanaCake <vanacake.gaming@gmail.com>
2020-11-03 12:16:46 -0800

    Add to list

--- app/src/main/java/com/example/my_events/MainActivity.java
+++ app/src/main/java/com/example/my_events/MainActivity.java
@@ -5,12 +5,23 @@ import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String SHARED_PREFERENCES_KEY = "shared_preferences";
+    private static final String EVENT_LIST_JSON = "event_list_key";
     RecyclerView recyclerView;
     RecyclerView.Adapter adapter;
     RecyclerView.LayoutManager layoutManager;
@@ -21,8 +32,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        events = new ArrayList<>();
-        getEvents();
+        //events = new ArrayList<>();
+        //getEvents();
+        loadEvents();
 
         recyclerView = (RecyclerView) findViewById(R.id.event_listRV);
         recyclerView.setHasFixedSize(true);
@@ -34,6 +46,38 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setAdapter(adapter);
         RecyclerView.ItemDecoration decoration = new DividerItemDecoration(this, LinearLayout.VERTICAL);
         recyclerView.addItemDecoration(decoration);
+    }
+
+    private void loadEvents() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFERENCES_KEY, Context.MODE_PRIVATE);
+        String eventListJson = sharedPreferences.getString(EVENT_LIST_JSON, null);
+        Type type = new TypeToken<ArrayList<Event>>(){}.getType();
+        Gson gson = new Gson();
+        events = gson.fromJson(eventListJson, type);
+        if(events == null) events = new ArrayList<Event>();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        saveEvents();
+    }
+
+    public void addEvent(View view){
+        EditText dateEditText = (EditText) findViewById(R.id.date_inputEditText);
+        EditText nameEditText = (EditText) findViewById(R.id.name_inputEditText);
+        String date = dateEditText.getText().toString();
+        String name = nameEditText.getText().toString();
+        events.add(new Event(date, name));
+        adapter.notifyDataSetChanged();
+    }
+    private void saveEvents(){
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFERENCES_KEY, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        Gson gson = new Gson();
+        String eventListJson = gson.toJson(events);
+        editor.putString(EVENT_LIST_JSON, eventListJson);
+        editor.apply();
     }
 
     public void getEvents(){
